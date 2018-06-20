bf20f3a5a96a05ce3959593569ca9d5aae6b543e
Plalith <38244468+Plalith@users.noreply.github.com>
2018-06-20 07:02:31 -0400

    Update cors.js

--- cors.js
+++ cors.js
@@ -1,7 +1,7 @@
 function crosPermission(){
     this.permission=function(req,res,next){
       res.header('Access-Control-Allow-Origin','*');
-      res.header('Access-Control-Allow-Headers','csrf');
+      res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
       res.header('Access-Control-Allow-Methods','GET','POST','PUT','DELETE','OPTIONS');
       next();
     }
